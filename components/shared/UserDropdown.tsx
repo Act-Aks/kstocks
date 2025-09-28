8e64791984897e8213e2527dba7a0c9416080f42
Act-Aks <akashsrivastava.gti@gmail.com>
2025-09-28 20:09:42 +0530

    feat: created dashboard ui

--- components/shared/UserDropdown.tsx
+++ components/shared/UserDropdown.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client'
 
-import {Avatar} from "@radix-ui/react-avatar";
-import {LogOutIcon} from "lucide-react";
-import {useRouter} from "next/navigation";
-import {NavItem} from "@/components/shared/NavItem";
-import {AvatarFallback, AvatarImage} from "@/components/ui/avatar";
-import {Button} from "@/components/ui/button";
+import { Avatar } from '@radix-ui/react-avatar'
+import { LogOutIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { NavItem } from '@/components/shared/NavItem'
+import { AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -13,91 +13,91 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {routes} from "@/lib/constants";
+} from '@/components/ui/dropdown-menu'
+import { routes } from '@/lib/constants'
 
 export const UserDropdown: React.FC = () => {
-    const router = useRouter();
+    const router = useRouter()
 
     function signOut() {
-        router.push(routes.SignIn);
+        router.push(routes.SignIn)
     }
 
     const user = {
-        name: "Kstocks Logo",
-        email: "kstocks@logo.com",
-    };
+        name: 'Kstocks Logo',
+        email: 'kstocks@logo.com',
+    }
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button
-                    variant={"ghost"}
+                    variant={'ghost'}
                     className={
-                        "flex items-center gap-3 text-gray-400 hover:text-yellow-500"
+                        'flex items-center gap-3 text-gray-400 hover:text-yellow-500'
                     }
                 >
-                    <Avatar className={"h-8 w-8"}>
-                        <AvatarImage src="https://github.com/shadcn.png" />
+                    <Avatar className={'h-8 w-8'}>
+                        <AvatarImage src='https://github.com/shadcn.png' />
                         <AvatarFallback
                             className={
-                                "bg-yellow-500 text-yellow-900 text-sm font-bold"
+                                'bg-yellow-500 font-bold text-sm text-yellow-900'
                             }
                         >
                             Aks
                         </AvatarFallback>
                     </Avatar>
-                    <div className={"hidden md:flex items-start"}>
-                        <span className={"text-base font-medium text-gray-400"}>
+                    <div className={'hidden items-start md:flex'}>
+                        <span className={'font-medium text-base text-gray-400'}>
                             {user.name}
                         </span>
                     </div>
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className={"text-gray-400"}>
+            <DropdownMenuContent className={'text-gray-400'}>
                 <DropdownMenuLabel>
-                    <div className={"flex relative items-center gap-3 py-2"}>
-                        <Avatar className={"h-10 w-10"}>
-                            <AvatarImage src="https://github.com/shadcn.png" />
+                    <div className={'relative flex items-center gap-3 py-2'}>
+                        <Avatar className={'h-10 w-10'}>
+                            <AvatarImage src='https://github.com/shadcn.png' />
                             <AvatarFallback
                                 className={
-                                    "bg-yellow-500 text-yellow-900 text-sm font-bold"
+                                    'bg-yellow-500 font-bold text-sm text-yellow-900'
                                 }
                             >
                                 Aks
                             </AvatarFallback>
                         </Avatar>
-                        <div className={"flex flex-col"}>
+                        <div className={'flex flex-col'}>
                             <span
                                 className={
-                                    "font-medium text-base text-gray-400"
+                                    'font-medium text-base text-gray-400'
                                 }
                             >
                                 {user.name}
                             </span>
-                            <span className={"text-gray-400 text-sm"}>
+                            <span className={'text-gray-400 text-sm'}>
                                 {user.email}
                             </span>
                         </div>
                     </div>
                 </DropdownMenuLabel>
-                <DropdownMenuSeparator className={"bg-gray-600"} />
+                <DropdownMenuSeparator className={'bg-gray-600'} />
                 <DropdownMenuItem
                     onClick={signOut}
                     className={
-                        "cursor-pointer font-medium text-gray-100 text-md transition-colors focus:bg-transparent focus:text-yellow-500"
+                        'cursor-pointer font-medium text-gray-100 text-md transition-colors focus:bg-transparent focus:text-yellow-500'
                     }
                 >
-                    <LogOutIcon className={"mr-2 hidden h-4 w-4 sm:block"} />
+                    <LogOutIcon className={'mr-2 hidden h-4 w-4 sm:block'} />
                     Logout
                 </DropdownMenuItem>
                 <DropdownMenuSeparator
-                    className={"hidden bg-gray-600 sm:block"}
+                    className={'hidden bg-gray-600 sm:block'}
                 />
-                <nav className={"sm:hidden"}>
+                <nav className={'sm:hidden'}>
                     <NavItem />
                 </nav>
             </DropdownMenuContent>
         </DropdownMenu>
-    );
-};
+    )
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs))
+}
+
+export function getWidgetScriptUrl(
+    widget: 'market-overview' | 'market-quotes' | 'stock-heatmap' | 'timeline',
+) {
+    return `https://s3.tradingview.com/external-embedding/embed-widget-${widget}.js`
 }
